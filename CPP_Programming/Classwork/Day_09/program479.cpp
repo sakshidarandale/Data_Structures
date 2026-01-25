81ec013c51d982f699584abbe7d00d18188352f1
Sakshi Ravindra Darandale <sakshidarandale8@gmail.com>
2026-01-25 21:51:20 +0530

    Add files via upload

--- CPP_Programming/Classwork/Day_09/program479.cpp
+++ CPP_Programming/Classwork/Day_09/program479.cpp
@@ -1,1 +1,96 @@
+#include <iostream>
+using namespace std;
 
+#pragma pack(1)
+class node
+{
+    public:
+    int data;
+    struct node *next;
+    struct node *prev;
+    
+    node(int no)
+    {
+        data=no;
+        next=NULL;
+        prev=NULL;
+    } 
+};
+
+class DoublyLLL
+{
+   public:
+   
+    node *first;
+    int iCount;
+    
+    DoublyLLL();
+    
+    void InsertFirst(int);
+    void InsertLast(int);
+    void InsertAtPos(int,int);
+    
+    void DeleteFirst();
+    void DeleteLast();
+    void DeleteAtPos(int);
+    
+    void Display();
+    int Count();
+ 
+};
+
+DoublyLLL :: DoublyLLL()
+{
+    
+}
+
+void DoublyLLL :: InsertFirst(int no)
+{
+    
+}
+
+void DoublyLLL :: InsertLast(int no)
+{
+    
+}
+
+void DoublyLLL :: InsertAtPos(int no,int pos)
+{
+    
+}
+
+void DoublyLLL :: DeleteFirst()
+{
+    
+}
+
+void DoublyLLL :: DeleteLast()
+{
+    
+}
+
+void  DoublyLLL ::DeleteAtPos(int pos)
+{
+    
+}
+
+void DoublyLLL :: Display()
+{
+    
+}
+
+int DoublyLLL :: Count()
+{
+    
+    return iCount;
+}
+
+int main()
+{
+    DoublyLLL *dobj = new DoublyLLL();
+    
+    
+    
+    
+    return 0;
+}
